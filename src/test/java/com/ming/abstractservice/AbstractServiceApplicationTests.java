6fd3aca3a986c31829d76d32a7cae1851f698d56
Minjae Kim <minjae.kim@mytrum.com>
2022-03-17 18:51:48 +0900

    Initial commit

---
+++ src/test/java/com/ming/abstractservice/AbstractServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ming.abstractservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
